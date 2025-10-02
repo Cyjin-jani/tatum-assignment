c7f31e7b4211de0df854cf5a6ef1332c78761cc8
yj Cho <57857367+Cyjin-jani@users.noreply.github.com>
2025-10-02 22:48:30 +0900

    [STEP 1] 프로젝트 환경설정 및 기반 구축 (#9)
    * chore: lint 및 prettier 세팅
    
    * chore: 필요 패키지 설치
    
    * feat: 폴더 구조 세팅
    
    * feat: query provider 세팅 수정
    
    * doc: pr 템플릿 추가

---
+++ eslint.config.ts
@@ -0,0 +1,44 @@
+import typescript from '@typescript-eslint/eslint-plugin';
+import typescriptParser from '@typescript-eslint/parser';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import prettier from 'eslint-plugin-prettier';
+
+export default [
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    languageOptions: {
+      parser: typescriptParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    plugins: {
+      '@typescript-eslint': typescript,
+      react,
+      'react-hooks': reactHooks,
+      prettier,
+    },
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off',
+      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+      'no-console': 'off',
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
+  {
+    ignores: ['node_modules/', '.next/', 'out/', 'build/', 'dist/'],
+  },
+];
