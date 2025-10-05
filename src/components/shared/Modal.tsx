a1c560cdfcba3cf18a58253398918089789c3e3c
Cyjin-jani <jinjanic91@gmail.com>
2025-10-05 13:25:55 +0900

    refactor: Modal 컴포넌트 수정

--- src/components/shared/Modal.tsx
+++ src/components/shared/Modal.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import * as DialogPrimitive from '@radix-ui/react-dialog';
 import { X } from 'lucide-react';
 
 import {
@@ -18,23 +17,25 @@ interface ModalProps {
   className?: string;
 }
 
-const Modal = React.forwardRef<
-  React.ElementRef<typeof DialogPrimitive.Content>,
-  ModalProps
->(({ open, onOpenChange, title, children, className, ...props }, ref) => (
+const Modal = ({
+  open,
+  onOpenChange,
+  title,
+  children,
+  className,
+  ...props
+}: ModalProps) => (
   <Dialog open={open} onOpenChange={onOpenChange}>
-    <DialogContent ref={ref} className={className} {...props}>
+    <DialogContent className={className} {...props}>
       <DialogHeader className="flex flex-row items-center justify-between">
         <DialogTitle className="text-left">{title}</DialogTitle>
-        <DialogClose className="ring-offset-background focus:ring-ring data-[state=open]:bg-accent data-[state=open]:text-muted-foreground rounded-sm opacity-70 transition-opacity hover:opacity-100 focus:ring-2 focus:ring-offset-2 focus:outline-none disabled:pointer-events-none">
+        <DialogClose className="data-[state=open]:bg-accent data-[state=open]:text-muted-foreground rounded-sm opacity-70 transition-opacity hover:cursor-pointer hover:opacity-100 focus:outline-none disabled:pointer-events-none">
           <X className="h-4 w-4" />
-          <span className="sr-only">Close</span>
         </DialogClose>
       </DialogHeader>
       <div className="flex-1">{children}</div>
     </DialogContent>
   </Dialog>
-));
-Modal.displayName = 'Modal';
+);
 
 export { Modal };
