8a4eced5c40c9dceb3f6711dccef6eb972796601
Cyjin-jani <jinjanic91@gmail.com>
2025-10-06 00:22:51 +0900

    fix: description이 없어 뜨는 warning 수정

--- src/components/shared/Modal.tsx
+++ src/components/shared/Modal.tsx
@@ -7,12 +7,14 @@ import {
   DialogHeader,
   DialogTitle,
   DialogClose,
+  DialogDescription,
 } from '@/components/ui/dialog';
 
 interface ModalProps {
   open: boolean;
   onOpenChange: (open: boolean) => void;
   title: string;
+  description?: string;
   children: React.ReactNode;
   className?: string;
 }
@@ -21,17 +23,23 @@ const Modal = ({
   open,
   onOpenChange,
   title,
+  description,
   children,
   className,
   ...props
 }: ModalProps) => (
   <Dialog open={open} onOpenChange={onOpenChange}>
     <DialogContent className={className} {...props}>
-      <DialogHeader className="flex flex-row items-center justify-between">
-        <DialogTitle className="text-left">{title}</DialogTitle>
-        <DialogClose className="data-[state=open]:bg-accent data-[state=open]:text-muted-foreground rounded-sm opacity-70 transition-opacity hover:cursor-pointer hover:opacity-100 focus:outline-none disabled:pointer-events-none">
-          <X className="h-4 w-4" />
-        </DialogClose>
+      <DialogHeader>
+        <div className="flex flex-row items-center justify-between">
+          <DialogTitle className="text-left">{title}</DialogTitle>
+          <DialogClose className="data-[state=open]:bg-accent data-[state=open]:text-muted-foreground rounded-sm opacity-70 transition-opacity hover:cursor-pointer hover:opacity-100 focus:outline-none disabled:pointer-events-none">
+            <X className="h-4 w-4" />
+          </DialogClose>
+        </div>
+        <DialogDescription className={description ? 'text-left' : 'sr-only'}>
+          {description || ' '}
+        </DialogDescription>
       </DialogHeader>
       <div className="flex-1">{children}</div>
     </DialogContent>
